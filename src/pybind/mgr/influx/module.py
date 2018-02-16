b491507c2802bd1ebc4c0b7b2e07db6bac81cfdf
Benjeman Meekhof <bmeekhof@users.noreply.github.com>
2018-02-16 11:47:36 -0500

    Clean up config handling and keep config with client objects for use in error msg or db create in send_to_influx

--- src/pybind/mgr/influx/module.py
+++ src/pybind/mgr/influx/module.py
@@ -47,7 +47,7 @@ class Module(MgrModule):
         'password': None,
         'interval': 5,
         'ssl': 'false',
-        'verify_ssl': 'true'
+        'verify_ssl': 'true',
         'destinations': None
     }
 
@@ -77,6 +77,7 @@ class Module(MgrModule):
     def get_df_stats(self):
         df = self.get("df")
         data = []
+        pool_info = {}
 
         df_types = [
             'bytes_used',
@@ -240,7 +241,8 @@ class Module(MgrModule):
         self.config[option] = value
 
     def init_module_config(self):
-         self.get_config("hostname", default=self.config_keys['hostname'])
+        self.config['hostname'] = \
+            self.get_config("hostname", default=self.config_keys['hostname'])
         self.config['port'] = \
             int(self.get_config("port", default=self.config_keys['port']))
         self.config['database'] = \
@@ -262,45 +264,52 @@ class Module(MgrModule):
         self.config['destinations'] = \
                 self.get_config_json("destinations")
 
-        if not self.config['hostname'] and not self.config['destinations']:
-            self.log.error("No Influx server configured, please set one using: "
-                           "ceph influx config-set mgr/influx/hostname <hostname> or ceph influx config-set mgr/influx/destinations '<json destinations>'")
-            return
-
         self.init_influx_clients()
 
     def init_influx_clients(self):
         self.clients = []
         
         if not self.config['destinations']:
-            self.log.debug("Sending data to Influx host: %s",
-                    self.config['hostname'])
-            client = InfluxDBClient(self.config['hostname'], self.config['port'],
-                    self.config['username'],
-                    self.config['password'],
-                    self.config['database'])
-            self.clients.append(client)
+            destinations = [ 
+                { 
+                    'hostname':   self.config['hostname'],
+                    'username':   self.config['username'],
+                    'password':   self.config['password'],
+                    'database':   self.config['database'],
+                    'ssl':        self.config['ssl'],
+                    'verify_ssl': self.config['verify_ssl']
+                },
+            ]
         else: 
-            for dest in self.config['destinations']:
-                merge_configs = [ 'port', 'database', 'username', 'password']
-                conf = dict()
+            destinations = self.config['destinations']
+        
+        for dest in destinations:
+            # use global settings if these keys not set in destinations object 
+            merge_configs = [ 'port', 'database', 'username', 'password', 'ssl', 'verify_ssl']
+            conf = dict()
 
-                for key in merge_configs:
-                    conf[key] = dest[key] if key in dest else self.config[key]
+            for key in merge_configs:
+                conf[key] = dest[key] if key in dest else self.config[key]
+                # make sure this is an int or may encounter type errors later
+                if key == 'port':
+                    conf[key] = int(conf[key])
 
-                self.log.debug("Sending data to Influx host: %s",
-                    dest['hostname'])
+            self.log.debug("Sending data to Influx host: %s",
+                dest['hostname'])
 
-                client = InfluxDBClient(dest['hostname'], int(conf['port']),
+            client = InfluxDBClient(dest['hostname'], conf['port'],
                 conf['username'], 
                 conf['password'], 
-                conf['database'] )
-                self.clients.append(client)
+                conf['database'],
+                conf['ssl'],
+                conf['verify_ssl'])
+
+            self.clients.append([client,conf])
 
     def send_to_influx(self):
-        if not self.config['hostname']:
-            self.log.error("No Influx server configured, please set one using: "
-                           "ceph influx config-set hostname <hostname>")
+        if not self.config['hostname'] and not self.config['destinations']:
+            self.log.error("No Influx server configured, please set using: "
+                           "ceph influx config-set mgr/influx/hostname <hostname> or ceph influx config-set mgr/influx/destinations '<json array>'")
             self.set_health_checks({
                 'MGR_INFLUX_NO_SERVER': {
                     'severity': 'warning',
@@ -314,23 +323,25 @@ class Module(MgrModule):
         daemon_stats = self.get_daemon_stats()
         pg_summary = self.get_pg_summary(df_stats[1])
 
-        for client in self.clients:
+        for client,conf in self.clients:
             # using influx client get_list_database requires admin privs,
             # instead we'll catch the not found exception and inform the user if
             # db can not be created
             try:
-                client.write_points(self.get_df_stats(), 'ms')
-                client.write_points(self.get_daemon_stats(), 'ms')
+                client.write_points(df_stats[0], 'ms')
+                client.write_points(daemon_stats, 'ms')
+                client.write_points(self.get_pg_summary(df_stats[1]))
                 self.set_health_checks(dict())
             except ConnectionError as e:
+                # InfluxDBClient also has get_host and get_port but since we have the config here anyways...
                 self.log.exception("Failed to connect to Influx host %s:%d",
-                                   self.config['hostname'], self.config['port'])
+                                   conf['hostname'], conf['port'])
                 self.set_health_checks({
                     'MGR_INFLUX_SEND_FAILED': {
                         'severity': 'warning',
                         'summary': 'Failed to send data to InfluxDB server at %s:%d'
                                    ' due to an connection error'
-                                   % (self.config['hostname'], self.config['port']),
+                                   % (conf['hostname'], conf['port']),
                         'detail': [str(e)]
                     }
                 })
@@ -339,9 +350,9 @@ class Module(MgrModule):
                     self.log.info("Database '%s' not found, trying to create "
                                   "(requires admin privs).  You can also create "
                                   "manually and grant write privs to user "
-                                  "'%s'", self.config['database'],
-                                  self.config['username'])
-                    client.create_database(self.config['database'])
+                                  "'%s'", conf['database'],
+                                  conf['username'])
+                    client.create_database(conf['database'])
                 else:
                     self.set_health_checks({
                         'MGR_INFLUX_SEND_FAILED': {
@@ -377,7 +388,7 @@ class Module(MgrModule):
             daemon_stats = self.get_daemon_stats()
             assert len(daemon_stats)
 
-            df_stats = self.get_df_stats()
+            df_stats = self.get_df_stats()[0]
 
             result = {
                 'daemon_stats': daemon_stats,
